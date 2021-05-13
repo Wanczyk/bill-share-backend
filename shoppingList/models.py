d4ffe1505cc92e49bc3fad7534541a78080be538
Martin Wanczyk <wanczyk.martin@gmail.com>
2021-05-13 21:15:45 +0200

    Add postgresql database and create docker-compose

--- shoppingList/models.py
+++ shoppingList/models.py
@@ -1,14 +1,15 @@
-import uuid
 from django.db import models
 from django.contrib.auth.models import User
 
 
 class ShoppingList(models.Model):
     name = models.CharField(max_length=20)
-    participants = models.JSONField()
+    participants = models.ManyToManyField(User)
 
 
 class Item(models.Model):
     item = models.CharField(max_length=20)
     description = models.CharField(max_length=50)
     shoppingList = models.ForeignKey(ShoppingList, on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    boughtBy = models.ForeignKey(User, on_delete=models.CASCADE)
