32b0452ece84cc19978199ff72b2a6181ce8c80b
Martin Wanczyk <wanczyk.martin@gmail.com>
2021-05-15 19:41:26 +0200

    add permissions and url for joining shoppingList

--- shoppingList/urls.py
+++ shoppingList/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path, include
-from .views import ShoppingListSet, ItemSet
+from .views import ShoppingListSet, ItemSet, ShoppingListJoin
 from rest_framework.routers import SimpleRouter
 
 router = SimpleRouter()
 router.register(r'shoppingList', ShoppingListSet, basename='shopping_list')
 router.register(r'item', ItemSet, basename='item')
 
-urlpatterns = router.urls
+urlpatterns = [
+    path('shoppingList/<int:pk>/join', ShoppingListJoin.as_view())
+]
+
+urlpatterns += router.urls
+
